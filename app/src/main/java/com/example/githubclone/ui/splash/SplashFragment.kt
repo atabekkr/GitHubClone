379894d6d7bd8e5b52350aced7eb2ffe310c8f5c
atabekkr <atabekdev@gmail.com>
2023-03-26 06:35:12 +0500

    added lottie animation

--- app/src/main/java/com/example/githubclone/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/githubclone/ui/splash/SplashFragment.kt
@@ -1,29 +1,31 @@
 package com.example.githubclone.ui.splash
 
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.githubclone.ui.MainActivity
 import com.example.githubclone.R
 import com.example.githubclone.data.local.LocalStorage
+import com.example.githubclone.databinding.FragmentSplashBinding
 import kotlinx.coroutines.delay
 
-class SplashFragment : Fragment(R.layout.fragment_splash){
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         lifecycleScope.launchWhenResumed {
-
-            delay(1000)
+            delay(2300)
             if (LocalStorage().isLogin) {
-                Log.d("TTTT", "ksajdfksjfio")
                 findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToContainerHomeFragment())
             } else {
-                Log.d("TTTT", "is logine5")
                 findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToFirstFragment())
             }
         }
